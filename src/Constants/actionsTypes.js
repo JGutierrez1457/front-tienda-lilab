40f17326c84904279d40fc969a4c05cfbde389b6
JGutierrez1457 <jgutierrezch@uni.pe>
2021-08-20 16:56:13 -0500

    Added possibles action types

---
+++ src/Constants/actionsTypes.js
@@ -0,0 +1,9 @@
+export const GET_ALL_ITEMS = 'GET_ALL_ITEMS';//Para recuperar todos los items
+export const BUY_ITEM = 'BUY_ITEM';//Para editar item por id, con la cantidad solicitada
+
+export const ADD_ITEM_CART = 'ADD_ITEM_CART'//Agregar item al carro
+export const EDIT_ITEM_CART = 'EDIT_ITEM_CART'//Editar (aumentar o disminuir cantidad) item al carro
+export const DELETE_ITEM_CART = 'DELETE_ITEM_CART'//Borrar item del carro
+export const CLEAR_CART = 'CLEAR_CART'//Limpiar carro (borrar todos los items)
+
+export const SET_FILTER = 'SET_FILTER';//Asignar filtro
