68988cd8942865c2559d4765adc01269650d28c6
JGutierrez1457 <jgutierrezch@uni.pe>
2021-08-20 18:23:27 -0500

    Hidded actions clear and purchase cart items when the cart is clear

--- src/Components/CartActions/CartActions.js
+++ src/Components/CartActions/CartActions.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import { Button} from '@material-ui/core'
+import { Button } from '@material-ui/core'
 
-function CartActions({ items, handleBuyItem, handleClearCart, handleCloseCart}) {
+function CartActions({ items, handleBuyItem, handleClearCart, handleCloseCart }) {
 
-    const onClearCart = ()=>{
+    const onClearCart = () => {
         handleClearCart();
     }
-    const onBuyCart = async ()=>{
-        for(let item of items){
-            await handleBuyItem(item.id, { quantity : item.quantity});//Edita cada item del carro por solicitud al api
+    const onBuyCart = async () => {
+        for (let item of items) {
+            await handleBuyItem(item.id, { quantity: item.quantity });//Edita cada item del carro por solicitud al api
         }
         handleClearCart();
     }
     return <>
-            
+        {!!items.length && <>
             <Button onClick={onClearCart}>Vaciar</Button>
             <Button onClick={onBuyCart}>Comprar</Button>
-            <Button onClick={handleCloseCart}>Cerrar</Button>
+        </>}
+        <Button onClick={handleCloseCart}>Cerrar</Button>
     </>
 }
 
